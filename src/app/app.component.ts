89b628346901686fc9dd6f5087d05a4beb2a92e3
Detlef von der Thuesen <dthuesen@gmx.de>
2017-06-06 20:56:14 +0200

    Fix app structure for components and services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,5 @@
 import { Component } from '@angular/core';
 
-import { DashboardComponent } from './dashboard/components/dashboard.component';
-
-import { ClientListComponent } from './clients/components/client-list.component';
-import { ClientDetailsComponent } from './clients/components/client-details.component';
-import { AddClientComponent } from './clients/components/add-client.component';
-import { EditClientComponent } from './clients/components/edit-client.component';
-
-import { AddGroupComponent } from './groups/components/add-group.component';
-import { EditGroupComponent } from './groups/components/edit-group.component';
-import { GroupListComponent } from './groups/components/group-list.component';
-
-import { ClientService } from './clients/services/client.service';
-import { GroupService } from './groups/services/group.service';
-
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+import { DashboardComponent } from './dashboard/components/dashboard.component';
+
 import{ ClientListComponent } from './clients/components/client-list.component';
 import{ ClientDetailsComponent } from './clients/components/client-details.component';
 import{ AddClientComponent } from './clients/components/add-client.component';
@@ -12,10 +14,14 @@ import{ AddGroupComponent } from './groups/components/add-group.component';
 import{ EditGroupComponent } from './groups/components/edit-group.component';
 import{ GroupListComponent } from './groups/components/group-list.component';
 
+import { ClientService} from './clients/services/client.service';
+import { GroupService} from './groups/services/group.service';
+
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     ClientListComponent,
     ClientDetailsComponent,
     AddClientComponent,
@@ -24,6 +30,10 @@ import{ GroupListComponent } from './groups/components/group-list.component';
     EditGroupComponent,
     GroupListComponent,
      ],
+    providers: [
+      ClientService,
+      GroupService
+    ]
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
